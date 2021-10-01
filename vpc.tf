960386d51abf7187e2448b88be6c794266fcc68d
mission113 <91178096+mission113@users.noreply.github.com>
2021-10-01 13:28:37 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,11 @@
 
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAU554LRDCM3OYPC4C"
+  secret_key = "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"
+ }
+resource "aws_vpc" "main" {
+  cidr_block = "10.32.0.0/16"
+}
+
+
