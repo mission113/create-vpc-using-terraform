ad5bde7e069481776ff9382d401958e155e76ce0
mission113 <91178096+mission113@users.noreply.github.com>
2021-10-04 15:17:21 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,34 @@
 
 provider "aws" {
-  region     = "us-west-2"
+  region     = "us-east-1"
   access_key = "AKIAU554LRDCM3OYPC4C"
   secret_key = "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"
  }
-resource "aws_vpc" "main" {
-  cidr_block = "10.32.0.0/16"
+resource "aws_instance" "myec2" {
+   ami = "ami-087c17d1fe0178315"
+   instance_type = "t2.micro"
 }
 
+resource "aws_eip" "lb" {
+  vpc      = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.myec2.id
+  allocation_id = aws_eip.lb.id
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "kplabs-security-group"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+
+#    cidr_blocks = [aws_eip.lb.public_ip/32]
+  }
+}
 
