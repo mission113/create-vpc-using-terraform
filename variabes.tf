da77208c0d233667632f6418c4a26aa1f092e61f
mission113 <91178096+mission113@users.noreply.github.com>
2021-10-05 21:48:03 +0530

    Update variabes.tf

--- variabes.tf
+++ variabes.tf
@@ -20,7 +20,7 @@ variable "availability_zone" {
 }
 variable "instance_ami" {
   description = "AMI for aws EC2 instance"
-  default = "ami-0cf31d971a3ca20d6"
+  default = "ami-087c17d1fe0178315"
 }
 variable "instance_type" {
   description = "type for aws EC2 instance"
