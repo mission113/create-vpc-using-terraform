38d8f741cbb64412c04f2383f794a34a60967982
mission113 <91178096+mission113@users.noreply.github.com>
2021-10-05 10:29:52 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
   secret_key = "c+SYpnJ+y8aXEv5xwT15ZAKIuAFrBLUAYWMjbhfK"
  }
 resource "aws_instance" "myec2" {
-   ami = "ami-0c2d06d50ce30b442"
+   ami = "ami-0f0965399835bd1e9"
    instance_type = "t2.micro"
 }
 
