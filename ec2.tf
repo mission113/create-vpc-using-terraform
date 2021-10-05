6306f401b836421c43267c041ea2974ca241fc5f
mission113 <91178096+mission113@users.noreply.github.com>
2021-10-05 22:16:40 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,38 +5,5 @@ provider "aws" {
   secret_key = "c+SYpnJ+y8aXEv5xwT15ZAKIuAFrBLUAYWMjbhfK"
  }
 resource "aws_vpc" "vpc" {
-  cidr_block = "${var.cidr_vpc}"
+  cidr_block = "10.16.0.0/16"
 }
-resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-}
-resource "aws_subnet" "subnet_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.cidr_subnet}"
-}
-resource "aws_route_table" "rtb_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
-route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.igw.id}"
-  }
-}
-resource "aws_route_table_association" "rta_subnet_public" {
-  subnet_id      = "${aws_subnet.subnet_public.id}"
-  route_table_id = "${aws_route_table.rtb_public.id}"
-}
-resource "aws_security_group" "sg_22" {
-  name = "sg_22"
-  vpc_id = "${aws_vpc.vpc.id}"
-  ingress {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-  resource "aws_instance" "myec2Instance" {
-  ami           = "${var.instance_ami}"
-  instance_type = "${var.instance_type}"
-  subnet_id = "${aws_subnet.subnet_public.id}"
-  vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
-  }
