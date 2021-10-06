098086216d8fd0650d7a4c238b0cd908ac726aaf
mission113 <91178096+mission113@users.noreply.github.com>
2021-10-06 15:59:07 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,19 +4,18 @@ provider "aws" {
   access_key = "AKIAU554LRDCM3OYPC4C"
   secret_key = "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"
  }
-data "aws_ami" "app_ami" {
+data "aws_ami" "ec2-replica" {
   most_recent = true
- 
+
   owners = ["self"]
 
 
-  filter {
+  tags = {
     name   = "ec2-replica"
     Tested = true
   }
 }
 
 resource "aws_instance" "instance-1" {
-    ami = data.aws_ami.app_ami.id
+    ami = data.aws_ami.ec2-replica.id
    instance_type = "t2.micro"
-}
