4a8137baaa67d8a96fd32d9849c84eb9e70a4540
mission113 <91178096+mission113@users.noreply.github.com>
2022-07-22 20:05:39 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,8 +1,8 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA6CTZHZX5YG5R5CRK"
-  secret_key = "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"
+  access_key = "AKIAXFXVCGST5EJGUNUA"
+  secret_key = "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"
  }
 resource "aws_instance" "instance-1" {
     ami = "ami-06640050dc3f556bb"
